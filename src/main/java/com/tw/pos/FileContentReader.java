83e61d69d11bf7077eaf1be12a96b0730107fc99
zhangtiny123 <tzhang920314@gmail.com>
2014-12-24 10:44:54 +0800

    init project and add file content reader and its test

---
+++ src/main/java/com/tw/pos/FileContentReader.java
@@ -0,0 +1,29 @@
+package com.tw.pos;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by taozhang on 12/24/14.
+ */
+public class FileContentReader {
+    public List<String> readData(String toReadFilePath) throws IOException {
+        File file = new File(toReadFilePath);
+        String temp = "";
+        List<String> resultList = new ArrayList<String>();
+
+        if(file.isFile() && file.exists()) {
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+
+            while ((temp = bufferedReader.readLine()) != null) {
+                resultList.add(temp);
+            }
+        }
+        else {
+            resultList.add("illegal input file path!");
+        }
+        return resultList;
+    }
+}
---
+++ src/test/java/com/tw/pos/FileContentReaderTest.java
@@ -0,0 +1,62 @@
+package com.tw.pos;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+
+/**
+ * Created by taozhang on 12/24/14.
+ */
+public class FileContentReaderTest {
+
+    //when
+
+    @Test
+    public void should_return_the_cart_info_when_given_the_cart_file_path() throws IOException {
+        //given
+        String filePath = "./src/main/resources/datas/cart.txt";
+        FileContentReader fReader = new FileContentReader();
+
+        //when
+        List<String> readResultList = fReader.readData(filePath);
+
+        //then
+        assertThat(readResultList.size(), is(7));
+        assertThat(readResultList.get(0), is("ITEM000001"));
+
+    }
+
+    @Test
+    public void should_return_file_not_find_when_given_illegal_file_path() throws IOException {
+        //given
+        String filePath = "./src/main/resources/datas/ffff.txt";
+        FileContentReader fReader = new FileContentReader();
+
+        //when
+        List<String> readResultList = fReader.readData(filePath);
+
+        //then
+        assertThat(readResultList.size(), is(1));
+        assertThat(readResultList.get(0), is("illegal input file path!"));
+
+    }
+
+    @Test
+    public void should_return_null_when_given_a_null_string() throws IOException {
+        //given
+        String filePath = "";
+        FileContentReader fReader = new FileContentReader();
+
+        //when
+        List<String> readResultList = fReader.readData(filePath);
+
+        assertThat(readResultList.size(), is(1));
+        assertThat(readResultList.get(0), is("illegal input file path!"));
+
+    }
+}
